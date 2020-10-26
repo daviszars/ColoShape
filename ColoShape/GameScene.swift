c7507fea79123cc5e7cafd03d26004d03be2d041
Davis Zarins <daviszarins@MacBook-Air.local>
2020-10-26 12:52:41 +0200

    initial commit

---
+++ ColoShape/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  ColoShape
+//
+//  Created by Davis Zarins on 21/10/2020.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+}
---
+++ ColoShape/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  ColoShape
+//
+//  Created by Davis Zarins on 21/10/2020.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return false
+    }
+}
